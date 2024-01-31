2d030dae2aca30d6232fa53e9f5cfc02f336ee5a
AlfioBattiato <battiato3@gmail.com>
2024-01-31 21:06:43 +0100

    modifiche varie css e js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let form = document.querySelector("form");
 const btnSubmit = document.getElementById("btn-submit");
-const secondDiv = document.getElementsByClassName("second-div");
+const secondDiv = document.getElementsByClassName("second-div")[0];
 const ul = document.querySelector("ul");
 
 form.onsubmit = function (e) {
@@ -8,19 +8,32 @@ form.onsubmit = function (e) {
 };
 
 const add = function () {
+   
   const input = document.getElementById("input-text");
   if (input.value !== "") {
-    const li = document.createElement("li");
-    li.textContent = input.value;
-    ul.appendChild(li);
+    const div = document.createElement("div")
+    div.classList.add("div");
+    secondDiv.appendChild(div);
+    const p = document.createElement("p");
+    p.innerText = input.value.toUpperCase();
+    div.appendChild(p);
+
     input.value = "";
     const btn = document.createElement("button");
-    btn.textContent = "x";
-    btn.style =
-      "color:white;background-color:red; border:none;margin-left:10px";
-    li.appendChild(btn);
-    btn.onclick = () => li.remove();
-    li.onclick = () => li.classList.add("line");
+    btn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+  </svg>`;
+
+    btn.classList.add("btn");
+    div.appendChild(btn);
+    btn.onclick = () => (p.remove(),btn.remove());
+    p.onclick = () => {
+      if (p.classList.contains("line")) {
+        p.classList.remove("line");
+      } else {
+        p.classList.add("line");
+      }
+    };
   }
 };
 
